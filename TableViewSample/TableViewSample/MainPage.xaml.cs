4b1b3d1832a2563eb5e6e7309f241b244da7a5f8
Christopher Palmer <chris.palmer@magnolia-river.com>
2020-07-28 10:37:17 -0500

    Add project files.

---
+++ TableViewSample/TableViewSample/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TableViewSample
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            for (var x = 0; x < 20; x++)
+            {
+                MainSection.Add(new ViewCell {View = new Label {Text = $"Added at {MainSection.Count}"}});
+            }
+        }
+
+        private void Button1_OnClicked(object sender, EventArgs e)
+        {
+            MainSection.Add(new ViewCell {View = new Label {Text = $"Added at {MainSection.Count}"}});
+        }
+
+        private void Button2_OnClicked(object sender, EventArgs e)
+        {
+            MainSection.Add(new ViewCell {View = new Entry {Text = $"Added at {MainSection.Count}"}});
+        }
+    }
+}
